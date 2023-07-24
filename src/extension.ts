ba83559ae91742bcf4fae4734edee8492b3387b2
jorrikklijnsma <jorrikklijnsma@gmail.com>
2023-07-24 12:21:32 +0200

    update

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,42 @@ export function activate(context: vscode.ExtensionContext) {
   let closeTabsToLeft = vscode.commands.registerCommand(
     'tabJanitor.closeTabsToLeft',
     async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        const currentViewColumn = editor.viewColumn;
-        if (currentViewColumn) {
-          vscode.window.showInformationMessage(
-            `Closing tabs to the left of ${currentViewColumn}`
-          );
-          vscode.window.visibleTextEditors.forEach(e => {
-            if (e.viewColumn && e.viewColumn < currentViewColumn) {
-              e.hide();
-            }
-          });
+      const activeEditor = vscode.window.activeTextEditor;
+      if (activeEditor) {
+        const activeGroup = vscode.window.tabGroups.activeTabGroup;
+        const activeTab = activeGroup?.activeTab;
+
+        if (activeGroup && activeTab) {
+          // vscode.window.showInformationMessage(
+          //   `Closing tabs to the left of ${activeTab.label}`
+          // );
+
+          vscode.window.showInformationMessage(`${activeGroup.viewColumn}`);
+          // for (const tab of activeGroup.tabs) {
+          //   if (tab.index < activeTab.index) {
+          //     await vscode.window.tabGroups.close(tab);
+          //   }
+          // }
         }
+
+        let activeTabIndex = activeGroup?.tabs.findIndex(
+          tab => tab === activeGroup?.activeTab
+        );
+
+        // if (
+        //   activeGroup &&
+        //   activeTabIndex !== undefined &&
+        //   activeTabIndex > -1
+        // ) {
+        //   vscode.window.showInformationMessage(
+        //     `Closing tabs to the left of ${activeGroup.activeTab?.label} tabIndex: ${activeTabIndex}`
+        //   );
+        //   while (activeTabIndex > 0) {
+        //     await vscode.window.tabGroups.close(activeGroup.tabs[0]); // Always close the first tab
+        //     activeTabIndex--; // Update the active tab's index
+        //     vscode.window.showInformationMessage(`tabIndex: ${activeTabIndex}`);
+        //   }
+        // }
       }
     }
   );
